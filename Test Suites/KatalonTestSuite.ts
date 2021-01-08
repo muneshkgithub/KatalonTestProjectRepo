b3eb26ad08886d0a9a8c67b25f57b0d0fae05566
muneshkgithub <choudhary.munesh@gmail.com>
2021-01-08 16:21:14 +0530

    suite is created

---
+++ Test Suites/KatalonTestSuite.ts
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>KatalonTestSuite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>d34f7709-53c1-4ff5-a8fa-5ebebac4cb63</testSuiteGuid>
+</TestSuiteEntity>
